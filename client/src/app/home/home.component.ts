de6ea724d257d653a88df13fdd4436b86c8e202e
dmitry <7520023@gmail.com>
2018-04-22 14:59:23 +0300

    feature: home screen was improved (also minor fixes to profile)

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -3,7 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: [
+    './home.component.css',
+    '../login/login-form.component.css',
+  ]
 })
 export class HomeComponent implements OnInit {
 
